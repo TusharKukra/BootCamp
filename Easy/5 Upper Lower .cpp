fd80a004d0f3bb985c2faa65193082fa8bb612c6
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2021-01-01 01:09:11 +0530

    Create 5 Upper Lower .cpp

---
+++ Easy/5 Upper Lower .cpp
@@ -0,0 +1,48 @@
+// IsLower
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    char c;
+    cin>>c;
+
+    if(islower(c)){
+        cout<<"Lowercase.";
+    }
+
+    else{
+        cout<<"Uppercase.";
+    }
+    
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////
+// IsUpper
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    char c;
+    cin>>c;
+
+    if(isupper(c)){
+        cout<<"Uppercase.";
+    }
+
+    else{
+        cout<<"Lowercase.";
+    }
+    
+    return 0;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+//Upper to Lower
+
