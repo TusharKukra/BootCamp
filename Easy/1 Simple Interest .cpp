cf75522aca0932f1679dab213b17f2d0e0b765e8
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2020-12-31 23:29:11 +0530

    Update 1 Simple Interest .cpp

--- Easy/1 Simple Interest .cpp
+++ Easy/1 Simple Interest .cpp
@@ -1,1 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main(){
+
+    float p,r,t,si;
+
+    cout<<"Enter Principle: ";
+    cin>>p;
+
+    cout<<endl<<"Enter Rate: ";
+    cin>>r;
+
+    cout<<endl<<"Enter Time: ";
+    cin>>t;
+
+    si = (p*r*t)/100.0;
+
+    cout<<endl<<"Simple Interest: "<<si;
+
+    return 0;
+}
