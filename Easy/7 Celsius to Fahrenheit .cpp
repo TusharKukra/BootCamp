2741290429de2812f0e1c813b0775aa766165fc6
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2021-01-02 16:23:58 +0530

    Create 7 Celsius to Fahrenheit .cpp

---
+++ Easy/7 Celsius to Fahrenheit .cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+     
+     float f,c;
+     cout<<"Enter temperature in celsius: ";
+     cin>>c;
+     
+     f = (9.0*c/5.0) + 32;
+     
+     cout<<endl<<"Temperature in Farenheit: "<<f;
+     
+	return 0;
+}
