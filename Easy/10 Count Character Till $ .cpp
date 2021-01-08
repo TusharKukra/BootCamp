5dea3a526d797a4a2d8fe3827e300a41e42e43bd
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2021-01-09 00:17:35 +0530

    Update 10 Count Character Till $ .cpp

--- Easy/10 Count Character Till $ .cpp
+++ Easy/10 Count Character Till $ .cpp
@@ -1,1 +1,25 @@
+#include<bits/stdc++.h>
 
+using namespace std;
+
+int main()
+{
+
+     // cpp program for counting characters in a string till $
+
+     string s;
+     cin>>s;
+
+     int count=0;
+     for(int i=0;s[i]!='$' && s[i]!='\0';i++){
+          if(s[i]!=' '){
+          
+          count++;
+         }
+        
+     }
+
+     cout<<count;
+
+     return 0;
+}
