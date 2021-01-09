befedc76ca0d5b691ba357b3ebaa368cdadf7ef5
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2021-01-10 00:35:39 +0530

    Create 13 Pattern Numbers & Star -2 .cpp

---
+++ Easy/13 Pattern Numbers & Star -2 .cpp
@@ -0,0 +1,31 @@
+Example :
+
+1******
+12*****
+123****
+1234***
+12345**
+123456*
+1234567
+
+
+Code : 
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+     int n,i,j;
+     cin>>n;
+
+     for(i=1;i<=n;i++)
+     {
+          for(j=1;j<=i;j++)
+               cout<<j;
+          for(;j<=n;j++)
+               cout<<"*";
+          cout<<"\n";
+     }
+}
