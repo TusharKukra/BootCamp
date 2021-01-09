ad9302eca1b3205714210a53fb1be51b168e6af7
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2021-01-09 22:08:15 +0530

    Update 11 Count WhiteSpaces and Words .cpp

--- Easy/11 Count WhiteSpaces and Words .cpp
+++ Easy/11 Count WhiteSpaces and Words .cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+   string s;
+   cout<<"Enter String: ";
+   getline(cin,s);
+
+   int count = 0;
+   int length = s.size();
+
+   for(int i=0;i<length;i++){
+        if(isspace(s.at(i))){    //int isspace ( int c );  (Checks whether c is a white-space character.)
+             count++;
+        }
+   }
+     int wordLength = length - count;
+   cout<<endl;
+
+     cout<<"Number of Spaces in the given string : "<<count<<endl;
+     cout<<"Number of words in the given string : "<<wordLength;
+    return 0;
+}
 
