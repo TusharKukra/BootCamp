9670873fe78026a264265cce0389ff84853c9161
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2021-01-11 23:51:53 +0530

    Update 2 Largest of N numbers .cpp

--- Easy/2 Largest of N numbers .cpp
+++ Easy/2 Largest of N numbers .cpp
@@ -27,3 +27,38 @@ int main(){
 
     return 0;
 }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Same as above (using Function)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int large(int a[], int n){
+
+     int max = a[0];
+     for(int i=0;i<n;i++){
+          
+          if(a[i] > max){
+               max = a[i];
+          }
+
+     }
+     return max;
+}
+
+int main(){
+
+     int n;
+     cin>>n;
+
+     int a[n];
+     for(int i=0;i<n;i++){cin>>a[i];}
+
+     int l = large(a,n);
+     cout<<"Largest : "<<l;
+
+
+     return 0;
+}
