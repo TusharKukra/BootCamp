3949615fde0cccd7daae064a3605d68bb493fd49
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2021-01-12 12:47:28 +0530

    Update 21 Binary Search (all methods)  .cpp

--- Easy/21 Binary Search (all methods)  .cpp
+++ Easy/21 Binary Search (all methods)  .cpp
@@ -56,10 +56,84 @@ int main(){
 }
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Method -2 (using Recursion)
 
+#include<bits/stdc++.h>
+using namespace std;
+
+// Using Recursion
+
+int binarySearch (int arr[] , int start, int end, int key){
+
+
+     if(start <= end){
+
+          int mid = start + (end - start)/2;
+
+          if(arr[mid] == key){
+               return mid;
+          } 
+
+          if(arr[mid] > key){
+               return binarySearch(arr,start, mid-1, key);
+          }
+
+          else{
+               return binarySearch(arr,mid+1,end,key);
+          }
+
+     }
+
+     return -1;
+
+}
+
+int main(){
+
+     int arr[] ={1,2,3,4,5,6,7};
+     int n = sizeof(arr)/sizeof(int);
+     int x;
+     cout<<"Enter Key : ";
+     cin>>x;
+
+     int result = binarySearch(arr,0,n-1,x);
+     
+     (result == -1) ? cout<<"Element is not present" : cout<<"Element is present at index: "<<result;
+
+     return 0;
+}
 
 
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Method -3 (using STL)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Using STL
+
+int main(){
+
+     int n;
+     cin>>n;
+     int a[n],i;
+     for(i=0;i<n;i++){
+          cin>>a[i];
+     }
+     int k;
+     cout<<"Enter Key : ";
+     cin>>k;
+
+     if(binary_search(a,a+n,k)){
+          
+          cout<<"Element found.";
+
+     }
+     else{
+          cout<<"Element not found.";
+     }
+     return 0;
+}
