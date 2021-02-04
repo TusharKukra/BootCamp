d7906c2a67e52596eeaae06d64b803730090d927
Tцѕндя Kцкяд <43209472+TusharKukra@users.noreply.github.com>
2021-02-04 21:04:50 +0530

    Update 7 Celsius to Fahrenheit .cpp

--- Easy/7 Celsius to Fahrenheit .cpp
+++ Easy/7 Celsius to Fahrenheit .cpp
@@ -12,3 +12,24 @@ int main() {
      
 	return 0;
 }
+
+
+//////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+using namespace std;
+
+//Fahrenheit to Celcius Program
+
+int main(){
+
+    int init, fval, step, f;
+    cin>>init>>fval>>step;
+    for(f=init; f<=fval ; f = f+ step){
+        int c = (5/9.0)*(f-32);
+        cout<<f<<" "<<c<<endl;
+    }
+    
+
+    return 0;
+}
