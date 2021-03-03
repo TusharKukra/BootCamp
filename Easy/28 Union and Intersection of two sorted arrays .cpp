a2cd129395072b4e302bec2c251755de8755079f
Tцѕндя Kцкяд <43209472+TusharKukra@users.noreply.github.com>
2021-03-03 15:44:25 +0530

    Update 28 Union and Intersection of two sorted arrays .cpp

--- Easy/28 Union and Intersection of two sorted arrays .cpp
+++ Easy/28 Union and Intersection of two sorted arrays .cpp
@@ -1,65 +1,66 @@
-#include <bits/stdc++.h> 
-using namespace std; 
-  
-/* Function prints Intersection of arr1[] and arr2[] 
-m is the number of elements in arr1[] 
-n is the number of elements in arr2[] */
-void printIntersection(int arr1[], int arr2[], int m, int n) 
-{ 
-    int i = 0, j = 0; 
-    while (i < m && j < n) { 
-        if (arr1[i] < arr2[j]) 
-            i++; 
-        else if (arr2[j] < arr1[i]) 
-            j++; 
-        else /* if arr1[i] == arr2[j] */
-        { 
-            cout << arr2[j] << " "; 
-            i++; 
-            j++; 
-        } 
-    } 
-}
-
-void printUnion(int arr1[], int arr2[], int m, int n) 
-{ 
-    int i = 0, j = 0; 
-    while (i < m && j < n) { 
-        if (arr1[i] < arr2[j]) 
-            cout << arr1[i++] << " "; 
-  
-        else if (arr2[j] < arr1[i]) 
-            cout << arr2[j++] << " "; 
-  
-        else { 
-            cout << arr2[j++] << " "; 
-            i++; 
-        } 
-    } 
-  
-    /* Print remaining elements of the larger array */
-    while (i < m) 
-        cout << arr1[i++] << " "; 
-  
-    while (j < n) 
-        cout << arr2[j++] << " "; 
-} 
-  
-int main() 
-{ 
-    int arr1[1001], arr2[1001];
-	int m,n;
-	cin>>m;
-	for(int i=0;i<m;i++){
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+	int n1;
+	cin>>n1;
+	int arr1[n1];
+	for(int i=0;i<n1;i++)
 		cin>>arr1[i];
-	}
-	cin>>n;
-	for(int i=0;i<n;i++){
+	int n2;
+	cin>>n2;
+	int arr2[n2];
+	for(int i=0;i<n2;i++)
 		cin>>arr2[i];
-	} 
-	
-	printUnion(arr1,arr2,m,n);
+	int i=0;
+	int j=0;
+	int ar1[n1+n2];
+	int k=0;
+	int m=0;
+	int ar2[n1];
+	for(i=0;i<n1;i++){
+		for(j=0;j<n2;j++){
+			if(arr1[i]==arr2[j]){
+				ar2[m]=arr1[i];
+				m++;
+				break;
+			}
+		}
+
+	}
+	// for(i=0;i<ar2;i++)
+	for(i=0;i<n1;i++){
+		ar1[i]=arr1[i];
+	}
+
+	j=0;
+	i=0;
+	while(j<max(n1,n2)){
+		for(int k=j;k<n2;k++){
+            //  NO NEED TO BREAK
+			// if(arr2[k]==ar1[j]){
+			//  break	
+			// }
+			if(k==n2-1){
+				ar1[n1+i]=arr2[j];
+				i++;
+			}
+        }
+		j++;
+	}
+	int c=n1+i;
+	sort(ar1,ar1+c);
+	cout<<ar1[0]<<" ";
+	for(j=1;j<c;j++){
+		if(ar1[j]!=ar1[j-1])
+			cout<<ar1[j]<<" ";
+	}
 	cout<<endl;
-    printIntersection(arr1, arr2, m, n); 
-    return 0; 
-} 
+	if(m>0){
+		cout<<ar2[0]<<" ";
+		for(j=1;j<m;j++){
+			if(ar2[j]!=ar2[j-1])
+				cout<<ar2[j]<<" ";
+		}
+	}
+	return 0;
+}
