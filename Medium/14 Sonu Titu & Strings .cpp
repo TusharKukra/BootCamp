c62615775e955caf76faf5afdf96d82e228be21d
Tцѕндя Kцкяд <43209472+TusharKukra@users.noreply.github.com>
2021-03-03 15:49:32 +0530

    Update 14 Sonu Titu & Strings .cpp

--- Medium/14 Sonu Titu & Strings .cpp
+++ Medium/14 Sonu Titu & Strings .cpp
@@ -1,3 +1,27 @@
+//Statement: 
+Sonu and Titu have two Strings. They want to make their strings anagrams of each other. They can either delete a character from string of Titu and add a character to the string of Sonu. Find the minimum number of operations required to make the strings anagrams of each other.
+
+Input Format
+First line of input contains number of test cases T. Each test case contains two lines. First line contains string of Sonu and second line contains string of Titu.
+
+Constraints
+0 <= N <= 10 Length of each string <= 10^5
+
+Output Format
+For each test case print the minimum number of operations required in a new line.
+
+Sample Input
+1
+abde
+edg
+
+Sample Output
+3
+
+
+/////////////////////////////////////////////////////////////////////////////
+//Code:
+
 #include<iostream>
 using namespace std;
 int main()
