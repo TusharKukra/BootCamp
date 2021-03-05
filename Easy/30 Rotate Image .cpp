c1f3e362413c8b4fb6fcc6786a99fd655ad3d822
Tцѕндя Kцкяд <43209472+TusharKukra@users.noreply.github.com>
2021-03-05 21:38:43 +0530

    Create 30 Rotate Image .cpp

---
+++ Easy/30 Rotate Image .cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void rotateWithoutSTL(int a[][1000], int n){
+
+    // for doing reverse of the each rows
+    for(int row=0; row<n; row++){
+        
+        int startcol = 0;
+        int endcol = n-1;
+
+        while(startcol < endcol){
+
+            swap(a[row][startcol] , a[row][endcol]);
+            startcol++;
+            endcol--;
+        }
+    }
+
+    //for doing transpose of the matrix
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+
+            if(i<j){
+            swap(a[i][j] , a[j][i]);
+            }
+        }
+    }
+
+}
+int main(){
+
+    int a[1000][1000];
+    int n;
+    cin>>n;
+    
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>a[i][j];
+        }
+    }
+
+    rotateWithoutSTL(a,n);
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
