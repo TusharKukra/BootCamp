a0901e718dcd838f0f0293f84ce45d4c1fd8ee66
Tцѕндя Kцкяд <43209472+TusharKukra@users.noreply.github.com>
2021-04-10 00:23:12 +0530

    Update 10 .cpp
    Imp Ques

--- Question Practice Sessions/10 .cpp
+++ Question Practice Sessions/10 .cpp
@@ -1,1 +1,5 @@
+// Given a String 'str' of digits, find the length of the longest substring of 'str' such that 
+// the length of the substring is 2k digits and sum of left k digits is 
+// equal to the sum of right k digits.
+
 
