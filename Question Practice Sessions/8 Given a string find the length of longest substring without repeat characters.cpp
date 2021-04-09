60b99a1bd5794a7421f4c3717b530cd59b385a8c
Tцѕндя Kцкяд <43209472+TusharKukra@users.noreply.github.com>
2021-04-09 21:57:28 +0530

    Update 8 Given a string find the length of longest substring without repeat characters.cpp

--- Question Practice Sessions/8 Given a string find the length of longest substring without repeat characters.cpp
+++ Question Practice Sessions/8 Given a string find the length of longest substring without repeat characters.cpp
@@ -61,7 +61,53 @@ int main(){
 
 //Method -2 (Sliding Window -Approach-1)
 
+#include<bits/stdc++.h>
+using namespace std;
 
+
+int main(){
+
+    string str;
+    cin>>str;
+
+    int n = str.length(); //str.size()
+    int max_len=0;
+
+    int left =0,right =0;
+
+    // i iterates over all the starting points of the substring
+    for(int i=0;i<n;i++){
+        int visited[256];  //created a map
+        for(int k=0;k<256;k++){
+            visited[i]=0;
+        }
+        // j iterates over all the ending points of the substring
+        for(int j=i;j<n;j++){
+            // str[i......j]
+            if(visited[str[j]]){
+                //reset & slide
+                break;
+            }
+            else{
+                //expand the window
+                int length = j-i+1;
+                if(length>max_len){
+                    max_len=length;
+                    left=i;
+                    right=j;
+                }
+                visited[str[j]]=1;
+            }   
+            
+        }
+    }
+    cout<<max_len<<endl;
+    for(int k=left;k<=right;k++){
+        cout<<str[k]<<"";
+    }
+    cout<<endl;
+    return 0;
+}
 
 
 //Method -3 (Sliding Window -Efficient Approach)
