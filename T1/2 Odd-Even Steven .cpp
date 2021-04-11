b49af626b3ddc7d92b766783f1603664812d9fd5
Tцѕндя Kцкяд <43209472+TusharKukra@users.noreply.github.com>
2021-04-11 22:53:16 +0530

    Update 2 Odd-Even Steven .cpp

--- T1/2 Odd-Even Steven .cpp
+++ T1/2 Odd-Even Steven .cpp
@@ -1,1 +1,22 @@
+/*
 
+Steven is trying a new operation over a string. The operation is that he will merge all the same 
+consecutive characters in a string e.g if a string is aabb , he will convert it to ab .
+
+Now, after doing this for a lot of strings, Steven observed an interesting thing that some of the strings
+became a palindrome. He gave these palindromic strings and the original string to his son.
+
+His son then asked him a difficult question, that how many palindromic sub-strings of even length and odd length
+could be formed fromed from the original string before and after merging operation. 
+
+
+Input Specs : The String (input string is made up of characters 'a' and 'b' .
+
+Output Specs : A 2 element array with the first element as the number of even length sun-strings and the second element as the number of odd length sub-strings.
+
+Eg :
+
+Input : aabb
+Output : {2,4}
+
+*/
