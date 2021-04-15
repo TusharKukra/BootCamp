bcb7a9a59e37d79fc0f9ced28ed7475722881ae9
Tцѕндя Kцкяд <43209472+TusharKukra@users.noreply.github.com>
2021-04-15 18:20:02 +0530

    Update 2 Coin Change .cpp

--- T1/Cpp-Codes/2 Coin Change .cpp
+++ T1/Cpp-Codes/2 Coin Change .cpp
@@ -8,11 +8,16 @@ determine how many ways change can be made for amount.
 There is a limitless supply of each coin type.
 */
 
-int countCoinChange(int n, int* coins, int m, int i){
+int countCoinChange(int n, int* coins, int m, int i, multiset<int>& ms){
 
     // base cases
     if(n==0){
         // i have found the coin change 
+        cout<<"{";
+        for(auto it = ms.begin(); it!=ms.end();it++){
+            cout<<*it<<" ";
+        }
+        cout<<"}";
         return 1;
     }
 
@@ -29,10 +34,12 @@ int countCoinChange(int n, int* coins, int m, int i){
     // i am standing at the ith coin & i have to make a decision.
 
     // 1. Include ith coin to the change configuration
-    int X = countCoinChange(n-coins[i], coins, m, i);
+    ms.insert(coins[i]);
+    int X = countCoinChange(n-coins[i], coins, m, i, ms);
 
     // 2. Do not include the ith coin to the change configuration
-    int Y = countCoinChange(n , coins, m, i+1);
+    ms.erase(ms.lower_bound(coins[i]));  //it will erase one occurrence of i
+    int Y = countCoinChange(n , coins, m, i+1, ms);
 
     return X+Y;
 }   
@@ -51,8 +58,11 @@ int main(){
     for(int i=0;i<m;i++){
         cin>>coins[i];
     }
+    // to print the configuration also : using multiset
 
-    cout<<countCoinChange(n,coins,m,0)<<endl;  // initially we will be at 0th coin so we'll decide further
+    multiset<int> ms;
+
+    cout<<countCoinChange(n,coins,m,0 ,ms)<<endl;  // initially we will be at 0th coin so we'll decide further
 
 
     return 0;
