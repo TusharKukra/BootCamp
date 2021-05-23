a2599b67b4d85df88e68422b9b6e82d4cb24d421
Tushar Kukra <43209472+TusharKukra@users.noreply.github.com>
2021-05-23 21:05:35 +0530

    Update 2. Nth character in Decrypted String .cpp

--- T2/2. Nth character in Decrypted String .cpp
+++ T2/2. Nth character in Decrypted String .cpp
@@ -1,5 +1,16 @@
 // Statement :
+Input: "a2b2c3", k = 5
+Output: c
+Decrypted string is "aabbccc"
 
+Input : "ab4c2ed3", k = 9
+Output : c
+Decrypted string is "ababababccededed"
+
+Input: "ab4c12ed3", k = 21
+Output: e
+Decrypted string is "ababababccccccccccccededed"
+  
 
 // Code :
 
