465b7f1e19f1a0c23c8ee198d7bcd7859e3ecab3
Tushar Kukra <43209472+TusharKukra@users.noreply.github.com>
2021-05-23 20:32:21 +0530

    Update 1. Iron Rod.cpp

--- T2/1. Iron Rod.cpp
+++ T2/1. Iron Rod.cpp
@@ -1,3 +1,23 @@
+// Statement :
+Iron rod of N inches, the cost of different sizes of iron rod is different, and is stored in an integer array.
+Your task is to help to portion rod in the best possible way such that he gets maximum money out of it.
+	
+Input1 : an integer value N representing the length of the rod in inches.
+Input2 : an integer array consisting of the cost of different lengths of iron (upto N inches) 
+	 nd has the starting index as 1 (i.e cost of 2 inches of iron rod = input2[2] )
+
+Output : Return the maximum profit he/she can make by selling his iron rod.
+	
+	
+Example :
+	I/P 1 : 5
+	I/P 2 : {0,2,1,7,3,5}
+	
+	O/P : 11
+		
+	Explaination : In this case, the max profit that can obtain by selling his rod is by dividing it into
+		       rods of length 1,1,3 so that it sums upto 2+2+7 = 11 . Hence, MaxProfit = 11.
+
 // Code :
 
 #include<bits/stdc++.h>
@@ -5,7 +25,7 @@ using namespace std;
 
 // Iron Rod Problem 
 
-int maxProfit(int a[], int n){
+int maxProfit(int n, int a[]){
 
 	int cost[n];
 
@@ -41,7 +61,7 @@ int main(){
 		cin>>a[i];
 	}
 
-	cout<<maxProfit(a,n)<<endl;
+	cout<<maxProfit(n,a)<<endl;
 
 	return 0;
 
