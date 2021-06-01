d32229321bcccb91db48b9f7fc13936e9e0565fc
Tushar Kukra <43209472+TusharKukra@users.noreply.github.com>
2021-06-01 16:49:41 +0530

    Update 3. Solving Expression .cpp

--- T2/3. Solving Expression .cpp
+++ T2/3. Solving Expression .cpp
@@ -6,143 +6,119 @@
 #include <bits/stdc++.h>
 using namespace std;
  
-// Function to find precedence of
-// operators.
+// Evaluating Expressions (Infix)
+
 int precedence(char op){
-    if(op == '+'||op == '-')
-    return 1;
-    if(op == '*'||op == '/')
-    return 2;
+    if(op=='+' || op=='-') return 1;
+    if(op=='*' || op=='/') return 2;
     return 0;
 }
- 
-// Function to perform arithmetic operations.
-int applyOp(int a, int b, char op){
-    switch(op){
-        case '+': return a + b;
-        case '-': return a - b;
-        case '*': return a * b;
-        case '/': return a / b;
-    }
-    return 1;
+
+int operatorApply(int a, int b, char op){
+    if(op=='+') return a+b;
+    else if(op=='-') return a-b;
+    else if(op=='*') return a*b;
+    else return a/b;
+
 }
- 
-// Function that returns value of
-// expression after evaluation.
+
 int evaluate(string tokens){
-    int i;
-     
-    // stack to store integer values.
-    stack <int> values;
-     
-    // stack to store operators.
-    stack <char> ops;
-     
-    for(i = 0; i < tokens.length(); i++){
-         
-        // Current token is a whitespace,
-        // skip it.
-        if(tokens[i] == ' ')
-            continue;
-         
-        // Current token is an opening
-        // brace, push it to 'ops'
-        else if(tokens[i] == '('){
+
+    // firstly we have to store integer values in 1 stack and operator values in another stack
+    stack<int> values;
+    stack<char> ops;
+
+    // now traverse the tokens and check wether :
+
+    for(int i=0;i<tokens.length();i++){
+
+        // if we got opening braces at first : push into ops stack
+        if(tokens[i]=='('){
             ops.push(tokens[i]);
         }
-         
-        // Current token is a number, push
-        // it to stack for numbers.
+
+        // if we got number : push it into values stack
         else if(isdigit(tokens[i])){
-            int val = 0;
-             
-            // There may be more than one
-            // digits in number.
-            while(i < tokens.length() &&
-                        isdigit(tokens[i]))
-            {
-                val = (val*10) + (tokens[i]-'0');
-                i++;
+            int val =0;
+
+            // what if we have a number more than one digit
+            while(i<tokens.length() && isdigit(tokens[i])){
+                val = (val * 10) + (tokens[i] - '0');
+                i++; 
             }
-             
+
             values.push(val);
-             
-            // right now the i points to
-            // the character next to the digit,
-            // since the for loop also increases
-            // the i, we would skip one
-            //  token position; we need to
-            // decrease the value of i by 1 to
-            // correct the offset.
-              i--;
+
+
+            // now the i points to the character next to the digit and the for loop also do i++ so we need to go back 1 step
+            i--;
         }
-         
-        // Closing brace encountered, solve
-        // entire brace.
-        else if(tokens[i] == ')')
-        {
-            while(!ops.empty() && ops.top() != '(')
-            {
+
+        // if we got an closing braces, then solve the entire brace
+        else if(tokens[i] == ')'){
+
+            while(!ops.empty() && ops.top()!='('){
+
                 int val2 = values.top();
                 values.pop();
-                 
+
                 int val1 = values.top();
                 values.pop();
-                 
+
                 char op = ops.top();
                 ops.pop();
-                 
-                values.push(applyOp(val1, val2, op));
+
+                values.push(operatorApply(val1,val2,op));
             }
-             
-            // pop opening brace.
-            if(!ops.empty())
-               ops.pop();
+
+            // also pop the opening brace, which corresponds to the closing brace
+            if(!ops.empty()){
+                ops.pop();
+            }
         }
-         
-        // Current token is an operator.
-        else
-        {
-            // While top of 'ops' has same or greater
-            // precedence to current token, which
-            // is an operator. Apply operator on top
-            // of 'ops' to top two elements in values stack.
-            while(!ops.empty() && precedence(ops.top())
-                                >= precedence(tokens[i])){
+
+
+
+        // if we got an operator while traversing token string
+        else{
+
+            // then , according to the precedence: 
+            // while top of ops has the same or greater precedence to current token, which is an operator.
+            // Apply operator on top of Ops stack to the top two elements of values stack.
+
+            while(!ops.empty() && precedence(ops.top()) >= precedence(tokens[i])){
                 int val2 = values.top();
                 values.pop();
-                 
+
                 int val1 = values.top();
                 values.pop();
-                 
+
                 char op = ops.top();
                 ops.pop();
-                 
-                values.push(applyOp(val1, val2, op));
+
+                values.push(operatorApply(val1,val2,op));
+                
             }
-             
-            // Push current token to 'ops'.
+
             ops.push(tokens[i]);
         }
+
     }
-     
-    // Entire expression has been parsed at this
-    // point, apply remaining ops to remaining
-    // values.
+
+    // now entire expression has been parsed at this point, apply remaining ops to remaining values.
     while(!ops.empty()){
         int val2 = values.top();
         values.pop();
-                 
+
         int val1 = values.top();
         values.pop();
-                 
+
         char op = ops.top();
         ops.pop();
-                 
-        values.push(applyOp(val1, val2, op));
+
+        values.push(operatorApply(val1,val2,op));
     }
-     
-    // Top of 'values' contains result, return it.
+
     return values.top();
 }
 
